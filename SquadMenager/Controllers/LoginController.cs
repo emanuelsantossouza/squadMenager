18488d1244495bff2a7bf841f78ef517016a6433
emanuel santoss <emanuel.jesus3@etec.sp.gov.br>
2023-05-02 14:43:36 -0300

    Adicionar arquivos de projeto.

---
+++ SquadMenager/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SquadMenager.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IndexLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ SquadMenager/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SquadMenager.Models
+{
+    public class UserViewModel
+    {
+        public string Usarname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
